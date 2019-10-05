e4a80922564fd0df9d10c896a189ff6d4abc66eb
ygqbasic <ygqbasic@gmail.com>
2019-10-05 13:29:37 +0800

    new theme the admin micro

---
+++ src/OrchardCore.Themes/TheAdminMicro/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "TheAdminMicro Theme",
+    Author = "The YFMicro Team",
+    Website = "https://orchardproject.net",
+    Version = "2.0.0",
+    Description = "The default Admin Micro theme.",
+    Tags = new [] { "admin" }
+)]
